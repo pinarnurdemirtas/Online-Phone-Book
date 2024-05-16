bca21cf6df078a3c648fd41a85d95cbf20b37106
pinarnurdemirtaş <pinardmrts18@gmail.com>
2024-05-16 22:42:13 +0300

    2

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    fetch('kullanicilar.json')
+    .then(response => response.json())
+    .then(data => {
+      kullanicilar = data;
+      console.log(kullanicilar); 
+    })
+    .catch(error => {
+      console.error('kullanicilar.json yüklenirken hata:', error);
+    });
    
+    
     document.getElementById('submit').addEventListener('click', function(event) {
         event.preventDefault(); 
 
-
-        var kullanicilar = [
-            {email:"engindemirog@gmail.com", password:1234},
-            {email:"pinardmrts@gmail.com", password:12345},
-            {email:"derindemir@gmail.com", password:12346},
-        ];
-
-        var twitler = [
-            {email: kullanicilar[0].email, twit:"enginin twiti"},
-            {email: kullanicilar[1].email, twit:"pınarın twiti"},
-            {email: kullanicilar[2].email, twit:"derinin twiti"},
-        ];
-
-       
         var email = document.getElementById('exampleInputEmail1').value;
         var password = document.getElementById('exampleInputPassword1').value;
 
         if (email.trim() == '' || password.trim() == '') {
-            document.getElementById('tepki').innerText = 'Lütfen tüm alanları doldurun.';
+            alert('Lütfen tüm alanları doldurun.');
             return;
         }
 
         for(var i = 0; i < kullanicilar.length; i++) {
-            console.log("Kullanıcı: ", kullanicilar[i].email, kullanicilar[i].password);
+            user = kullanicilar[i]
+            console.log("Kullanıcı: ", user.email, user.password);
             console.log("Girilen: ", email, password);
-            if(email == kullanicilar[i].email && password == kullanicilar[i].password) {
-                document.getElementById('tepki').innerText = twitler[i].twit;
+            if(email == user.email && password == user.password) {
+                document.getElementById('tepki').innerText = user.name + " Twiti";
                 return;
             } 
-            else {
-                document.getElementById('tepki').innerText = 'Geçersiz e-posta veya şifre.';
-            }}
+        } alert('Geçersiz e-posta veya şifre.');
+    });
+
+    document.getElementById('submit2').addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+        var name = document.getElementById('exampleInputName1').value;
+        var email = document.getElementById('exampleInputEmail2').value;
+        var password = document.getElementById('exampleInputPassword2').value;
+
+        if (name.trim() == '' || email.trim() == '' || password.trim() == '') {
+            alert('Lütfen tüm alanları doldurun.');
+            return;
+        }
+
+        kullanicilar.push({name: name, email: email, password: password});
+        console.log(kullanicilar);
+        alert('Yeni kullanıcı eklendi.');
+        
     });
 });
+
